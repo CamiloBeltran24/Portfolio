24c600833efff2bde9299090f9171a353e1dfc4c
Camilo Beltran Fonseca <cambelf.dev@gmail.com>
2021-06-03 10:39:39 -0500

    he agregado la interactividad al menu, ahora este es capas de mostrarse y ocultarse, tambien se cambio el color de borde inferior de los items de skills y en el menu del sitio.

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,3 +1,23 @@
-function desplazar_menu(elemento){
-  
+let menu = document.getElementById("menu");
+let menu_list = document.querySelector("#menu-list");
+let burguer_menu = document.getElementById("burguerMenu_icon");
+let closer = document.getElementById("close_button");
+let menu_buttons = document.querySelectorAll(".menu_buttons");
+
+// Funciones
+function desplazar_menu() {
+  console.log("Click");
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    menu.classList.add("hidde");
+  } else {
+    menu.classList.remove("hidde");
+    menu.classList.add("active");
+  }
 }
+// FIN Funciones
+
+burguer_menu.addEventListener("click", desplazar_menu);
+closer.addEventListener("click", desplazar_menu);
+
+menu_list.addEventListener("click", desplazar_menu);
