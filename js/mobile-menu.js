2ff7ae2072055a63fe4dcd0909d870681b42ae14
Camilo Beltran Fonseca <cambelf.dev@gmail.com>
2021-06-17 11:27:06 -0500

    Correccion de comportamiento del menu responsive en vista desktop

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,19 +5,19 @@ let closer = document.getElementById("close_button");
 let menu_buttons = document.querySelectorAll(".menu_buttons");
 
 // Funciones
-function desplazar_menu() {
+function muestra_menu(e) {
   console.log("Click");
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
-    menu.classList.add("hidde");
-  } else {
-    menu.classList.remove("hidde");
-    menu.classList.add("active");
-  }
+  menu.classList.remove("hidde");
+  menu.classList.add("active");
+}
+function oculta_menu(e) {
+  console.log("click en: ", e.target);
+  menu.classList.remove("active");
+  menu.classList.add("hidde");
 }
 // FIN Funciones
 
-burguer_menu.addEventListener("click", desplazar_menu);
-closer.addEventListener("click", desplazar_menu);
+burguer_menu.addEventListener("click", muestra_menu);
+closer.addEventListener("click", oculta_menu);
 
 menu_list.addEventListener("click", desplazar_menu);
